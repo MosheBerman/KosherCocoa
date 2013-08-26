709f75a70ed3f739b9029059b2089f4033a1cbef
Moshe <moshberm@gmail.com>
2013-08-26 11:19:17 -0400

    Added a category on NSDate for convenient dates.

---
+++ KosherCocoa/Library/Categories/Foundation/NSDate/NSDate+ConvenientDates.h
@@ -0,0 +1,44 @@
+/**
+ *  NSDate+ConvenientDates.h
+ *  KosherCocoa 2
+ *
+ *  Created by Moshe Berman on 8/26/13.
+ *  Updated by Moshe Berman on 8/26/13.
+ *
+ *  Use of KosherCocoa 2 is governed by the LGPL 2.1 License.
+ */
+
+#import <Foundation/Foundation.h>
+
+@interface NSDate (ConvenientDates)
+
+/**-----
+ * @name Creating Hebrew Dates
+ * -----
+ */
+
+/** This method creates a date given a hebrew month and day.
+ *
+ *  The year is taken from the year component of `[NSDate date]` against 
+ *  the Hebrew calendar.
+ *
+ *  @param month The month of the year. 1 is Tishrei, 2 Heshvan, etc.
+ *  @param day The day of the month.
+ *  @return An NSDate initialized with the supplied parameters.
+ */
+
++ (NSDate *)dateWithHebrewMonth:(NSInteger)month andDay:(NSInteger)day;
+
+/** This method creates a date given a hebrew month and day.
+ *
+ *  The year is taken from the value of `[NSDate date]`.
+ *
+ *  @param month The month of the year. 1 is Tishrei, 2 Heshvan, etc.
+ *  @param day The day of the month.
+ *  @param year The hebrew year number.
+ *  @return An NSDate initialized with the supplied parameters.
+ */
+
++ (NSDate *)dateWithHebrewMonth:(NSInteger)month andDay:(NSInteger)day andYear:(NSInteger)year;
+
+@end
---
+++ KosherCocoa/Library/Categories/Foundation/NSDate/NSDate+ConvenientDates.m
@@ -0,0 +1,63 @@
+/**
+ *  NSDate+ConvenientDates.m
+ *  KosherCocoa 2
+ *
+ *  Created by Moshe Berman on 8/26/13.
+ *  Updated by Moshe Berman on 8/26/13.
+ *
+ *  Use of KosherCocoa 2 is governed by the LGPL 2.1 License.
+ */
+
+#import "NSDate+ConvenientDates.h"
+#import "NSCalendar+Components.h"
+
+@implementation NSDate (ConvenientDates)
+
+/**
+ *  This method creates a date given a hebrew month and day.
+ *
+ *  The year is taken from the value of `[NSDate date]`.
+ *
+ *  @param month The month of the year. 1 is Tishrei, 2 Heshvan, etc.
+ *  @param day The day of the month.
+ *  @return An NSDate initialized with the supplied parameters.
+ */
+
++ (NSDate *)dateWithHebrewMonth:(NSInteger)month andDay:(NSInteger)day {
+    
+    NSCalendar *hebrewCalendar = [[NSCalendar alloc] initWithCalendarIdentifier:NSHebrewCalendar];
+    
+    NSUInteger year = [hebrewCalendar yearsInDate:[NSDate date]];
+    
+    return [self dateWithHebrewMonth:month andDay:day andYear:year];
+}
+
+/**
+ *  This method creates a date given a hebrew month and day.
+ *
+ *  The year is taken from the value of `[NSDate date]`.
+ *
+ *  @param month The month of the year. 1 is Tishrei, 2 Heshvan, etc.
+ *  @param day The day of the month.
+ *  @param year The hebrew year number.
+ *  @return An NSDate initialized with the supplied parameters.
+ */
+
++ (NSDate *)dateWithHebrewMonth:(NSInteger)month andDay:(NSInteger)day andYear:(NSInteger)year {
+    
+    NSDateComponents *comps = [[NSDateComponents alloc] init];
+    
+    NSCalendar *hebrewCalendar = [[NSCalendar alloc] initWithCalendarIdentifier:NSHebrewCalendar];
+    
+    [comps setCalendar:hebrewCalendar];
+    
+    [comps setMonth:month];
+    [comps setDay:day];
+    [comps setYear:year];
+    
+    NSDate *retVal = [hebrewCalendar dateFromComponents:comps];
+    
+    return retVal;
+}
+
+@end
