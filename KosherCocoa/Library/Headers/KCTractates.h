009e5034b8535bc7c621aa8b915f597d66464d2b
Moshe <moshberm@gmail.com>
2013-08-27 20:32:06 -0400

    Added a Tractates header to the library.

---
+++ KosherCocoa/Library/Headers/KCTractates.h
@@ -0,0 +1,61 @@
+/**
+ *  KCTractates.h
+ *  KosherCocoa 2
+ *
+ *  Created by Moshe Berman on 8/27/13.
+ *  Updated by Moshe Berman on 8/27/13.
+ *
+ *  Use of KosherCocoa 2 is governed by the LGPL 2.1 License.
+ */
+
+#ifndef KosherCocoa_KCTractates_h
+#define KosherCocoa_KCTractates_h
+
+/** kTractate
+ *
+ *  An enum representing the tractates of the Babylonian Talmud.
+ */
+enum kTractate {
+    kTractateBerachos = 0,
+    kTractateShabbos,
+    kTractateEruvin,
+    kTractatePesachim,
+    kTractateShekalim,
+    kTractateYoma,
+    kTractateSukkah,
+    kTractateBeitzah,
+    kTractateRoshHashana,
+    kTractateTaanis,
+    kTractateMegillah,
+    kTractateMoedKatan,
+    kTractateChagigah,
+    kTractateYevamos,
+    kTractateKesubos,
+    kTractateNedarim,
+    kTractateNazir,
+    kTractateSotah,
+    kTractateGitin,
+    kTractateKiddushin,
+    kTractateBavaKamma,
+    kTractateBavaMetzia,
+    kTractateBavaBatra,
+    kTractateSanhedrin,
+    kTractateMakkos,
+    kTractateShevuos,
+    kTractateAvodahZarah,
+    kTractateHoriyos,
+    kTractateZevachim,
+    kTractateMenachos,
+    kTractateChulin,
+    kTractateBechoros,
+    kTractateArachin,
+    kTractateTemurah,
+    kTractateKerisos,
+    kTractateMeilah,
+    kTractateKinnim,
+    kTractateTamid,
+    kTractateMidos,
+    kTractateNiddah
+};
+
+#endif
