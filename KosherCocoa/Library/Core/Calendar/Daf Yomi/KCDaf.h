bbec2d8cad96f800c245cad94db5bf6b308536de
Moshe <moshberm@gmail.com>
2013-10-07 23:48:57 -0400

    Documented the KCDaf class.

--- KosherCocoa/Library/Core/Calendar/Daf Yomi/KCDaf.h
+++ KosherCocoa/Library/Core/Calendar/Daf Yomi/KCDaf.h
@@ -23,21 +23,60 @@
  */
 
 /**
- *  The index of the tractate.
+ *  The index of the tractate. 
  *
- *  @see KCTractates.h
+ *  The index can be translated into a string 
+ *  by calling displayName or displayNameTransliterated.
+ *
+ *  @see displayName
+ *  @see displayNameTransliterated
  */
 
 @property NSInteger tractateIndex;
 
 /**
- *
+ *  The page number that the given Daf represents.
  */
+
 @property NSInteger pageNumber;
 
+/**-----
+ *  @name Instantiating a Daf.
+ * -----
+ */
+
+/**
+ *  This method instantiates a Daf with an index and page number.
+ *
+ *  For valid index values and what they represent, see KCTractates.h
+ *
+ *  @param tractateIndex The index of the tractate.
+ *  @param number The page number.
+ *  @return An instance of KCDaf.
+ */
 - (id) initWithTractateIndex:(NSInteger)tractateIndex andPageNumber:(NSInteger)number;
 
-- (NSString *)mesechtaName;
+/**-----
+ *  @name Displaying a Tractate
+ * -----
+ */
+
+/**
+ *  This method returns the dispolay name of a given tractate 
+ *  as a Hebrew string.
+ *
+ *  @return A string with hebrew name of the tractate that corresponds to the tractateIndex.
+ */
+
+- (NSString *)displayName;
+
+/**
+ *  This method returns the dispolay name of a given tractate
+ *  as a transliterated Hebrew string.
+ *
+ *  @return A string with transliterated name of the tractate that corresponds to the tractateIndex.
+ */
+- (NSString *)displayNameTransliterated;
 
 
 @end
--- KosherCocoa/Library/Core/Calendar/Daf Yomi/KCDaf.m
+++ KosherCocoa/Library/Core/Calendar/Daf Yomi/KCDaf.m
@@ -1,5 +1,5 @@
 /**
- *  Daf.m
+ *  KCDaf.m
  *  KosherCocoa 2
  *
  *  Created by Moshe Berman on 11/13/11.
@@ -23,14 +23,16 @@
     return self;
 }
 
-- (NSString *)mesechtaName {
+- (NSString *)displayName
+{
     NSArray *names = @[@"\u05D1\u05E8\u05DB\u05D5\u05EA", @"\u05E9\u05D1\u05EA", @"\u05E2\u05D9\u05E8\u05D5\u05D1\u05D9\u05DF", @"\u05E4\u05E1\u05D7\u05D9\u05DD", @"\u05E9\u05E7\u05DC\u05D9\u05DD", @"\u05D9\u05D5\u05DE\u05D0", @"\u05E1\u05D5\u05DB\u05D4", @"\u05D1\u05D9\u05E6\u05D4", @"\u05E8\u05D0\u05E9 \u05D4\u05E9\u05E0\u05D4"@"\u05EA\u05E2\u05E0\u05D9\u05EA", @"\u05DE\u05D2\u05D9\u05DC\u05D4",@"\u05DE\u05D5\u05E2\u05D3 \u05E7\u05D8\u05DF", @"\u05D7\u05D2\u05D9\u05D2\u05D4",  @"\u05D9\u05D1\u05DE\u05D5\u05EA", @"\u05DB\u05EA\u05D5\u05D1\u05D5\u05EA", @"\u05E0\u05D3\u05E8\u05D9\u05DD", @"\u05E0\u05D6\u05D9\u05E8", @"\u05E1\u05D5\u05D8\u05D4", @"\u05D2\u05D9\u05D8\u05D9\u05DF", @"\u05E7\u05D9\u05D3\u05D5\u05E9\u05D9\u05DF", @"\u05D1\u05D1\u05D0 \u05E7\u05DE\u05D0", @"\u05D1\u05D1\u05D0 \u05DE\u05E6\u05D9\u05E2\u05D0", @"\u05D1\u05D1\u05D0 \u05D1\u05EA\u05E8\u05D0", @"\u05E1\u05E0\u05D4\u05D3\u05E8\u05D9\u05DF", @"\u05DE\u05DB\u05D5\u05EA", @"\u05E9\u05D1\u05D5\u05E2\u05D5\u05EA", @"\u05E2\u05D1\u05D5\u05D3\u05D4 \u05D6\u05E8\u05D4", @"\u05D4\u05D5\u05E8\u05D9\u05D5\u05EA", @"\u05D6\u05D1\u05D7\u05D9\u05DD", @"\u05DE\u05E0\u05D7\u05D5\u05EA", @"\u05D7\u05D5\u05DC\u05D9\u05DF", @"\u05D1\u05DB\u05D5\u05E8\u05D5\u05EA", @"\u05E2\u05E8\u05DB\u05D9\u05DF", @"\u05EA\u05DE\u05D5\u05E8\u05D4", @"\u05DB\u05E8\u05D9\u05EA\u05D5\u05EA", @"\u05DE\u05E2\u05D9\u05DC\u05D4", @"\u05EA\u05DE\u05D9\u05D3", @"\u05E7\u05D9\u05E0\u05D9\u05DD", @"\u05DE\u05D9\u05D3\u05D5\u05EA", @"\u05E0\u05D3\u05D4"];
     
     return names[[self tractateIndex]];
 }
 
 
-- (NSString *)mesechtaNameTransliterated {
+- (NSString *)displayNameTransliterated
+{
     NSArray *names = @[@"Berachos", @"Shabbos", @"Eruvin", @"Pesachim", @"Shekalim", @"Yoma", @"Sukkah", @"Beitzah", @"Rosh Hashana", @"Taanis", @"Megillah", @"Moed Katan", @"Chagigah", @"Yevamos", @"Kesubos", @"Nedarim", @"Nazir", @"Sotah", @"Gitin", @"Kiddushin", @"Bava Kamma", @"Bava Metzia", @"Bava Basra", @"Sanhedrin", @"Makkos", @"Shevuos", @"Avodah Zarah", @"Horiyos", @"Zevachim", @"Menachos", @"Chullin", @"Bechoros", @"Arachin", @"Temurah", @"Kerisos", @"Meilah", @"Kinnim", @"Tamid", @"Midos", @"Niddah"];
     
     return names[[self tractateIndex]];
