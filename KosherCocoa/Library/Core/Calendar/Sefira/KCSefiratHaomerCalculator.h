84d85b31c189a81c618049e491433e230b184e9d
Moshe <moshberm@gmail.com>
2013-10-08 00:07:46 -0400

    Renamed KCSefiratHaomer to KCSefiratHaomerCalculator.

---
+++ KosherCocoa/Library/Core/Calendar/Sefira/KCSefiratHaomerCalculator.h
@@ -0,0 +1,72 @@
+/**
+ *  KCSefiratHaomerCalculator.h
+ *  KosherCocoa 2
+ *
+ *  Created by Moshe Berman on 3/1/11.
+ *  Updated by Moshe Berman on 10/i/13.
+ *
+ *  Use of KosherCocoa 2 is governed by the LGPL 2.1 License.
+ */
+
+#import <Foundation/Foundation.h>
+
+/** A class that calculates Sefirat Haomer. */
+
+@interface KCSefiratHaomerCalculator : NSObject
+
+/* -----
+ * @name Check if a date falls during sefira 
+ * -----
+ */
+
+/**
+ *  This method checks if [NSDate date] represents a day
+ *  that falls out during the omer count on the Hebrew
+ *  calendar.
+ *
+ *  @return YES if today is during the sefira count, otherwise NO.
+ */
+
++ (BOOL)fallsToday;
+
+/**
+ *  This method checks if the supplied date represents a
+ *  day that dalls out during the omer count according
+ *  to the Hebrew calendar.
+ *
+ *  @param date an NSDate
+ *  @return YES if date is during the sefira count, otherwise NO.
+ */
+
++ (BOOL)fallsOnDate:(NSDate *)date;
+
+/* -----
+ *  @name Getting the day of sefira.
+ * -----
+ */
+
+/**
+ *  This method returns an integer representing the
+ *  current day of the omer count.
+ *
+ *  If [NSDate date] does not fall out during the 
+ *  omer count, then 0 is returned.
+ *
+ *  @return An integer representing the day of the omer count.
+ */
+
++ (NSInteger)dayOfSefira;
+
+/**
+ *  This method returns an integer representing the
+ *  day of the omer count which date falls out on.
+ *
+ *  If date does not fall out during the
+ *  omer count, then 0 is returned.
+ *
+ *  @return An integer representing the day of the omer count.
+ */
+
++ (NSInteger)dayOfSefiraForDate:(NSDate *)date;
+
+@end
---
+++ KosherCocoa/Library/Core/Calendar/Sefira/KCSefiratHaomerCalculator.m
@@ -0,0 +1,65 @@
+/**
+ *  KCSefiratHaomerCalculator.m
+ *  KosherCocoa 2
+ *
+ *  Created by Moshe Berman on 3/1/11.
+ *  Updated by Moshe Berman on 10/8/13.
+ *
+ *  Use of KosherCocoa 2 is governed by the LGPL 2.1 License.
+ */
+
+#import "KCSefiratHaomerCalculator.h"
+#import "MBCalendarCategories.h"
+
+//  There are always 49 days in the sefira count
+const NSInteger lengthOfSefira = 49;
+
+@implementation KCSefiratHaomerCalculator
+
+#pragma mark - Ranges
+
+/* Checking If a Date Falls During Sefira */
+
++ (BOOL)fallsToday
+{
+    return [KCSefiratHaomerCalculator fallsOnDate:[NSDate date]];
+}
+
+//  YES if date is during the omer, else NO
++ (BOOL)fallsOnDate:(NSDate *)date
+{
+    return [KCSefiratHaomerCalculator dayOfSefiraForDate:date] != 0;
+}
+
+#pragma mark - Day of Sefira
+
+/* Get Day of Sefira */
+
++ (NSInteger)dayOfSefira
+{
+	return [KCSefiratHaomerCalculator dayOfSefiraForDate:[NSDate date]];
+}
+
+// Returns 1-49, or 0 for all other days
++ (NSInteger)dayOfSefiraForDate:(NSDate *)date
+{
+	NSDate *firstDayOfTheOmer = [KCSefiratHaomerCalculator _dateOfSixteenNissanForYearOfDate:date];
+    
+    NSCalendar *hebrewCalendar = [[NSCalendar alloc] initWithCalendarIdentifier:NSHebrewCalendar];
+    
+    return MAX(MIN([hebrewCalendar daysFromDate:firstDayOfTheOmer toDate:date], lengthOfSefira), 0);
+}
+
+#pragma mark - Utility Method
+
++ (NSDate *)_dateOfSixteenNissanForYearOfDate:(NSDate *)date
+{
+	NSCalendar *hebrewCalendar = [[NSCalendar alloc] initWithCalendarIdentifier:NSHebrewCalendar];
+	
+    NSInteger hebrewYearInDate = [hebrewCalendar yearsInDate:date];
+	
+    //  Foundation reserves 7 for Adar II, so Nissan is 8
+	return [NSDate dateWithDay:16 Month:8 Year:hebrewYearInDate andCalendar:hebrewCalendar];
+}
+
+@end
