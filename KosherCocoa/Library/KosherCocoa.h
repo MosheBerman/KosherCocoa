ca9ef3015b25e58a0571b04cff709f39477ce129
Moshe <moshberm@gmail.com>
2013-10-13 12:07:00 -0400

    Updated the comments in KosherCocoa.h

--- KosherCocoa/Library/KosherCocoa.h
+++ KosherCocoa/Library/KosherCocoa.h
@@ -1,14 +1,18 @@
-//
-//  KosherCocoa.h
-//  Gabbai
-//
-//  Created by Moshe Berman on 8/17/12.
-//
-//
+/**
+ *  KosherCocoa.h
+ *  KosherCocoa 2
+ *
+ *  Created by Moshe Berman on 8/17/13.
+ *  Updated by Moshe Berman on 10/11/13.
+ *
+ *  Use of KosherCocoa 2 is governed by the LGPL 2.1 License.
+ */
 
 #ifndef KosherCocoa_KosherCocoa_h
 #define KosherCocoa_KosherCocoa_h
 
-#import
+/*
+ *  TODO: Import the core headers here. 
+ */
 
 #endif
