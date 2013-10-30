9387cb08bd04dfbd1632ec696c0aa1974c2e3a2a
Moshe <moshberm@gmail.com>
2013-10-30 13:19:15 -0400

    Added a calendar category to the project and repository so we can compile with forwarded methods.

---
+++ KosherCocoa/Library/Categories/KosherCocoa/KCAstronomicalCalendar+DateManipulation.h
@@ -0,0 +1,41 @@
+/**
+ *  KCZmanimCalendar+DateManipulation.h
+ *  KosherCocoa 2
+ *
+ *  Created by Moshe Berman on 10/30/13.
+ *  Updated by Moshe Berman on 10/30/13.
+ *
+ *  Use of KosherCocoa 2 is governed by the LGPL 2.1 License.
+ */
+
+#import "KCAstronomicalCalendar.h"
+
+/**
+ *  These method prototypes allow us to compile.
+ *  We'll forward these to the internal calendar
+ *  object that's equiped to handle the calls.
+ */
+
+@interface KCAstronomicalCalendar (DateManipulation)
+
+#pragma mark - Add Units
+
+- (NSDate *)dateByAddingSeconds:(NSUInteger)seconds toDate:(NSDate *)date;
+- (NSDate *)dateByAddingMinutes:(NSUInteger)minutes toDate:(NSDate *)date;
+- (NSDate *)dateByAddingHours:(NSUInteger)hours toDate:(NSDate *)date;
+- (NSDate *)dateByAddingDays:(NSUInteger)days toDate:(NSDate *)date;
+- (NSDate *)dateByAddingWeeks:(NSUInteger)weeks toDate:(NSDate *)date;
+- (NSDate *)dateByAddingMonths:(NSUInteger)months toDate:(NSDate *)date;
+- (NSDate *)dateByAddingYears:(NSUInteger)years toDate:(NSDate *)date;
+
+#pragma mark - Subtract Units
+
+- (NSDate *)dateBySubtractingSeconds:(NSUInteger)seconds fromDate:(NSDate *)date;
+- (NSDate *)dateBySubtractingMinutes:(NSUInteger)minutes fromDate:(NSDate *)date;
+- (NSDate *)dateBySubtractingHours:(NSUInteger)hours fromDate:(NSDate *)date;
+- (NSDate *)dateBySubtractingDays:(NSUInteger)days fromDate:(NSDate *)date;
+- (NSDate *)dateBySubtractingWeeks:(NSUInteger)weeks fromDate:(NSDate *)date;
+- (NSDate *)dateBySubtractingMonths:(NSUInteger)months fromDate:(NSDate *)date;
+- (NSDate *)dateBySubtractingYears:(NSUInteger)years fromDate:(NSDate *)date;
+
+@end
---
+++ KosherCocoa/Library/Categories/KosherCocoa/KCAstronomicalCalendar+DateManipulation.m
@@ -0,0 +1,31 @@
+//
+//  KCZmanimCalendar+DateManipulation.m
+//  KosherCocoa
+//
+//  Created by Moshe Berman on 10/30/13.
+//  Copyright (c) 2013 Moshe Berman. All rights reserved.
+//
+
+#import "KCAstronomicalCalendar+DateManipulation.h"
+
+@implementation KCAstronomicalCalendar (DateManipulation)
+
+- (NSDate *)dateByAddingSeconds:(NSUInteger)seconds toDate:(NSDate *)date{ return nil; }
+- (NSDate *)dateByAddingMinutes:(NSUInteger)minutes toDate:(NSDate *)date{ return nil; }
+- (NSDate *)dateByAddingHours:(NSUInteger)hours toDate:(NSDate *)date{ return nil; }
+- (NSDate *)dateByAddingDays:(NSUInteger)days toDate:(NSDate *)date{ return nil; }
+- (NSDate *)dateByAddingWeeks:(NSUInteger)weeks toDate:(NSDate *)date{ return nil; }
+- (NSDate *)dateByAddingMonths:(NSUInteger)months toDate:(NSDate *)date{ return nil; }
+- (NSDate *)dateByAddingYears:(NSUInteger)years toDate:(NSDate *)date{ return nil; }
+
+#pragma mark - Subtract Units
+
+- (NSDate *)dateBySubtractingSeconds:(NSUInteger)seconds fromDate:(NSDate *)date{ return nil; }
+- (NSDate *)dateBySubtractingMinutes:(NSUInteger)minutes fromDate:(NSDate *)date{ return nil; }
+- (NSDate *)dateBySubtractingHours:(NSUInteger)hours fromDate:(NSDate *)date{ return nil; }
+- (NSDate *)dateBySubtractingDays:(NSUInteger)days fromDate:(NSDate *)date{ return nil; }
+- (NSDate *)dateBySubtractingWeeks:(NSUInteger)weeks fromDate:(NSDate *)date{ return nil; }
+- (NSDate *)dateBySubtractingMonths:(NSUInteger)months fromDate:(NSDate *)date{ return nil; }
+- (NSDate *)dateBySubtractingYears:(NSUInteger)years fromDate:(NSDate *)date{ return nil; }
+
+@end
