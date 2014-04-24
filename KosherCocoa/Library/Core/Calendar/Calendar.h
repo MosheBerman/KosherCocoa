dbbba9503d28f9f79008ee339fd827fb551681d6
Moshe <moshberm@gmail.com>
2014-04-24 18:25:43 -0400

    Added a Calendar header to the project.

---
+++ KosherCocoa/Library/Core/Calendar/Calendar.h
@@ -0,0 +1,34 @@
+//
+//  Calendar.h
+//  KosherCocoa
+//
+//  Created by Moshe Berman on 4/23/14.
+//  Copyright (c) 2014 Moshe Berman. All rights reserved.
+//
+
+#ifndef KosherCocoa_Calendar_h
+#define KosherCocoa_Calendar_h
+
+/* Zmanim */
+#import "KCZmanimCalendar.h"
+#import "KCComplexZmanimCalendar.h"
+
+/* Sefira */
+#import "KCSefiratHaomerCalculator.h"
+#import "KCSefiraFormatter.h"
+
+/* Parasha */
+#import "KCParashatHashavuaCalculator.h"
+#import "KCParasha.h"
+
+/* Sefira */
+#import "KCSefiraFormatter.h"
+#import "KCSef  iratHaomerCalculator.h"
+
+/* Daf Yomi */
+#import "KCDafYomiCalculator.h"
+
+/* Chagim */
+#import "KCJewishCalendar.h"
+
+#endif
