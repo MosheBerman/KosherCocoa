325aaa5b83329244712b5cb5bfcf4aa4428cfb1f
Moshe <moshberm@gmail.com>
2014-06-08 06:40:12 -0400

    Added an empty class to represent a Jewish Holiday.

---
+++ KosherCocoa/Library/Core/Calendar/Chagim/KCJewishHoliday.h
@@ -0,0 +1,13 @@
+//
+//  KCJewishHoliday.h
+//  KosherCocoa
+//
+//  Created by Moshe Berman on 6/8/14.
+//  Copyright (c) 2014 Moshe Berman. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface KCJewishHoliday : NSObject
+
+@end
---
+++ KosherCocoa/Library/Core/Calendar/Chagim/KCJewishHoliday.m
@@ -0,0 +1,13 @@
+//
+//  KCJewishHoliday.m
+//  KosherCocoa
+//
+//  Created by Moshe Berman on 6/8/14.
+//  Copyright (c) 2014 Moshe Berman. All rights reserved.
+//
+
+#import "KCJewishHoliday.h"
+
+@implementation KCJewishHoliday
+
+@end
