7e157ac1dbaa66e37a03d35a109d6b06a6f02cba
Moshe Berman <moshberm@gmail.com>
2015-11-11 23:58:49 -0500

    Commit new KosherCocoa header.

---
+++ KosherCocoa/KosherCocoa.h
@@ -0,0 +1,31 @@
+//
+//  KosherCocoa.h
+//  KosherCocoa
+//
+//  Created by Moshe Berman on 11/11/15.
+//  Copyright © 2015 Moshe Berman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KosherCocoa.
+FOUNDATION_EXPORT double KosherCocoaVersionNumber;
+
+//! Project version string for KosherCocoa.
+FOUNDATION_EXPORT const unsigned char KosherCocoaVersionString[];
+
+#import <KosherCocoa/KCDafYomiCalculator.h>
+
+#import <KosherCocoa/KCComplexZmanimCalendar.h>
+#import <KosherCocoa/KCZmanimCalendar.h>
+
+#import <KosherCocoa/KCJewishCalendar.h>
+#import <KosherCocoa/KCJewishHoliday.h>
+#import <KosherCocoa/KCParashatHashavuaCalculator.h>
+#import <KosherCocoa/KCParasha.h>
+#import <KosherCocoa/KCSefiraFormatter.h>
+#import <KosherCocoa/KCSefiratHaomerCalculator.h>
+
+#import <KosherCocoa/KCGeoLocation.h>
+#import <KosherCocoa/KCAstronomicalCalculator.h>
+#import <KosherCocoa/KCSunriseAndSunsetCalculator.h>    /* NOAA Algorithms */
