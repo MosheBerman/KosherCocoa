fb8e4f2e0b85af8f8ee64268a5dd52b9fb25b35d
Moshe Berman <moshefromsinai@gmail.com>
2016-04-28 14:17:47 -0400

    Added OS X and tvOS targets. Closes  #14.

---
+++ KosherCocoaTVOS/KosherCocoaTVOS.h
@@ -0,0 +1,19 @@
+//
+//  KosherCocoaTVOS.h
+//  KosherCocoaTVOS
+//
+//  Created by Moshe Berman on 4/28/16.
+//  Copyright © 2016 Moshe Berman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KosherCocoaTVOS.
+FOUNDATION_EXPORT double KosherCocoaTVOSVersionNumber;
+
+//! Project version string for KosherCocoaTVOS.
+FOUNDATION_EXPORT const unsigned char KosherCocoaTVOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KosherCocoaTVOS/PublicHeader.h>
+
+
