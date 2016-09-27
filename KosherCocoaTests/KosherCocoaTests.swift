471efed6b3d396587fe1acbc59bd0fdb7afe9532
Moshe Berman <moshefromsinai@gmail.com>
2016-09-27 17:54:18 -0400

    Began implementing tests.

--- KosherCocoaTests/KosherCocoaTests.swift
+++ KosherCocoaTests/KosherCocoaTests.swift
@@ -20,4 +20,59 @@ class KosherCocoaTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
+    
+    // MARK: - KCZMan Tests
+    
+    func testForMetadataWithMissingEntries()
+    {
+        // Iterate and check all fields for zero length strings
+    }
+    
+    func testForMethodNames()
+    {
+        // Iterate and check for stringcs containing the word "method" or a pair of parenthesis'()'
+    }
+    
+    func testForDuplicates()
+    {
+        // Iterate groups and check for strings that appear more than once
+        
+        let originalMetadata = KCZman.relatedZmanimMapping()
+        
+        var mapping : [String: Int] = [:]
+        
+        // We could use reduce here...
+        for group in originalMetadata
+        {
+            for zman in group
+            {
+                if let count = mapping[zman]
+                {
+                    mapping[zman] = count + 1
+                }
+                else
+                {
+                    mapping[zman] = 1
+                }
+            }
+        }
+        
+        
+        for tuple in mapping
+        {
+            XCTAssert(tuple.value == 1, "\(tuple.key) was \(tuple.value). Expected 1.")
+        }
+        
+    }
+    
+    func testForMissingMetadataEntry()
+    {
+        // Iterate groups of methods and check for missing metadata entries
+    }
+    
+    func testForExtraMetadata()
+    {
+        // Iterate metadata and check for groups that aren't represented in the selectors grouping.
+    }
 }
+
