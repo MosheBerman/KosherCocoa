31377dc0f297f5dbd4ce2c1da0d9a94a8def31cd
MosheBerman <mosheberman@users.noreply.github.com>
2018-11-25 21:30:29 -0500

    Add empty location controller class.

---
+++ KosherCocoa/Library/Utilities/Location/KCLocationController.h
@@ -0,0 +1,25 @@
+//
+//  KCLocationController.h
+//  KosherCocoa
+//
+//  Created by Moshe Berman on 11/25/18.
+//  Copyright © 2018 Moshe Berman. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+
+/**
+ KCLocationController provides a central interface for apps to perform the following location-based operations:
+ - Use CoreLocation to detect the user's location.
+ - Maintain a list of one or more locations for use with Ultimate Zmanim.
+ - 
+ 
+ */
+@interface KCLocationController : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ KosherCocoa/Library/Utilities/Location/KCLocationController.m
@@ -0,0 +1,13 @@
+//
+//  KCLocationController.m
+//  KosherCocoa
+//
+//  Created by Moshe Berman on 11/25/18.
+//  Copyright © 2018 Moshe Berman. All rights reserved.
+//
+
+#import "KCLocationController.h"
+
+@implementation KCLocationController
+
+@end
