bdceef26815fbf5908d1d2a51dc8323cced418b1
MosheBerman <mosheberman@users.noreply.github.com>
2018-11-26 18:22:04 -0500

    Implement location controller.

--- KosherCocoa/Library/Utilities/Location/KCLocationController.h
+++ KosherCocoa/Library/Utilities/Location/KCLocationController.h
@@ -7,18 +7,53 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <CoreLocation/CoreLocation.h>
+#import "KCLocationTypes.h"
 
 NS_ASSUME_NONNULL_BEGIN
 
+@class KCGeoLocation;
 
 /**
- KCLocationController provides a central interface for apps to perform the following location-based operations:
+ KCLocationController provides a central interface for apps to perform the following location-related operations:
  - Use CoreLocation to detect the user's location.
- - Maintain a list of one or more locations for use with Ultimate Zmanim.
- - 
- 
+ - Maintain a list of one or more locations for use with the framework.
+ - Convert between Core Location and KCGeoLocation.
  */
 @interface KCLocationController : NSObject
+
+// MARK: - User Core Location to detect the user's location.
+
+/**
+ Updates the location using CoreLocation. Prompts for permission if necessary.
+
+ @param completion A completion hander
+ */
+- (void)automaticLocationWithCompletion:(KCLocationUpdateCompletionBlock)completion;
+
+// MARK: - Maintain a list of locations
+
+/**
+ Access array of locations that the user has persisted to disk.
+ */
+@property (strong, nonatomic) NSArray <KCGeoLocation *> *persistedLocations;
+
+// TODO: Document keying mechanism for these two methods, once determined.
+
+/**
+ Persists a geolocation to disk. 
+
+ @param geoLocation The location object to persist.
+ */
+- (void)persistLocation:(KCGeoLocation *)geoLocation;
+
+
+/**
+ Deletes a location object from the persisted store.
+ 
+ @param geoLocation The object to delete.
+ */
+- (void)deletePersistedLocation:(KCGeoLocation *)geoLocation;
 
 @end
 
--- KosherCocoa/Library/Utilities/Location/KCLocationController.m
+++ KosherCocoa/Library/Utilities/Location/KCLocationController.m
@@ -7,7 +7,95 @@
 //
 
 #import "KCLocationController.h"
+#import "KCPersistence.h"
+
+NSString * persistedLocationsKey = @"com.zmanimkit.persisted-locations";
+
+@interface KCLocationController () <CLLocationManagerDelegate>
+
+/**
+ The internal location manager object.
+ */
+@property (strong, nonatomic) CLLocationManager *locationManager;
+
+/**
+ A persistent store.
+ */
+@property (strong, nonatomic) KCPersistence *store;
+@end
 
 @implementation KCLocationController
+
+// MARK: - Initializer
+
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        _locationManager = [[CLLocationManager alloc] init];
+        _store = [[KCPersistence alloc] init];
+    }
+    return self;
+}
+
+
+// MARK: - User Core Location to detect the user's location.
+
+/**
+ Updates the location using CoreLocation. Prompts for permission if necessary.
+ 
+ @param completion A completion hander
+ */
+- (void)automaticLocationWithCompletion:(KCLocationUpdateCompletionBlock)completion;
+{
+    
+}
+
+// MARK: - CLLocationManagerDelegate
+
+- (void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray<CLLocation *> *)locations {
+    
+}
+
+// MARK: - Maintain a list of locations
+
+/**
+ Access array of locations that the user has persisted to disk.
+ */
+- (nonnull NSArray <KCGeoLocation *> *)persistedLocations;
+{
+    NSArray <KCGeoLocation *> * locations = (NSArray <KCGeoLocation *> *)[self.store objectForKey:persistedLocationsKey];
+    if (!locations) {
+        locations = @[];
+    }
+    return locations;
+}
+
+// TODO: Document keying mechanism for these two methods, once determined.
+
+/**
+ Persists a geolocation to disk.
+ 
+ @param geoLocation The location object to persist.
+ */
+- (void)persistLocation:(KCGeoLocation *)geoLocation;
+{
+    NSArray <KCGeoLocation *> * persistedLocations = [self.persistedLocations arrayByAddingObject:geoLocation];
+    [self.store persistObject:persistedLocations forKey:persistedLocationsKey];
+}
+
+/**
+ Deletes a location object from the persisted store.
+ 
+ @param geoLocation The object to delete.
+ */
+- (void)deletePersistedLocation:(KCGeoLocation *)geoLocation;
+{
+    NSMutableArray *locations = self.persistedLocations.mutableCopy;
+    [locations removeObject:geoLocation];
+    [self.store persistObject:locations forKey:persistedLocationsKey];
+}
+
+
 
 @end
