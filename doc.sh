841361497fecd3e49baeb266d6aa1367461d5054
MosheBerman <mosheberman@users.noreply.github.com>
2019-03-29 10:30:21 -0400

    Add script to generate docs.

---
+++ doc.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+echo '>>> Switching to project root dir.'
+cd ~/code/ios/koshercocoa/;
+
+echo '>>> Preparing Project'
+echo '    - Copying source to temporary directory.'
+cp -r KosherCocoa/ temp/;
+
+echo '    - Switching to temporary directory.'
+cd temp;
+
+
+echo '    - Renaming `Library` to `KosherCocoa`, to match umbrella header.'
+mv Library/ KosherCocoa/;
+cd KosherCocoa;
+
+echo '    - Flattening directory.'
+find ./ -mindepth 2 -type f -exec mv '{}' ./ ';'
+
+echo '    - Moving back up to parent directory.'
+cd ~/code/ios/koshercocoa/temp;
+
+echo '>>> Running Jazzy'
+# export SOURCEKIT_LOGGING=3;
+jazzy \
+  --objc \
+  --author "Moshe Berman" \
+  --clean \
+  --umbrella-header ./KosherCocoa.h \
+  --framework-root ./ \
+  --output ../docs \
+  --readme ../README.md \
+  --github-url https://github.com/MosheBerman/Koshercocoa
+
+echo ' >>> Cleaning up...'
+echo '    - Moving back up to parent directory.'
+cd ../; 
+echo '    - Removing tempprary directory.'
+rm -rf ./temp;
+echo 'Done!'
