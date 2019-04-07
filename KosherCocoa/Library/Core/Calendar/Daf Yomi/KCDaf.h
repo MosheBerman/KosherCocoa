12b67d71b1858c502fceffa4727fedfc09b67718
MosheBerman <mosheberman@users.noreply.github.com>
2019-04-07 12:17:24 -0400

    Clean up KCDaf.

--- KosherCocoa/Library/Core/Calendar/Daf Yomi/KCDaf.h
+++ KosherCocoa/Library/Core/Calendar/Daf Yomi/KCDaf.h
@@ -15,63 +15,55 @@
 
 /**
  This class represents a folio of the talmud.
+ 
+ A folio is a two-sided page. (i.e. Pages 2a and 2b together make up a folio.)
  */
 NS_SWIFT_NAME(Daf)
 @interface KCDaf : NSObject
 
-// MARK: - Accessing a *Daf*
-
 /**
- *  The index of the tractate. 
- *
- *  The index can be translated into a string 
- *  by calling displayName or displayNameTransliterated.
- *
- *  @see name
- *  @see nameTransliterated
+ The index of the tractate.
+ 
+ The index can be translated into a string by calling `displayName` or `displayNameTransliterated`.
+ 
+ @see displayName
+ @see displayNameTransliterated
  */
-
 @property NSInteger tractateIndex;
 
 /**
- *  The page number that the given Daf represents.
+ The page number that the given Daf represents.
  */
-
 @property NSInteger pageNumber;
 
 
 // MARK: - Instantiating a Daf
 
 /**
- *  This method instantiates a Daf with an index and page number.
- *
- *  For valid index values and what they represent, see KCTractates.h
- *
- *  @param tractateIndex The index of the tractate.
- *  @param number The page number.
- *  @return An instance of KCDaf.
+ This method instantiates a Daf with an index and page number.
+ 
+ For valid index values and what they represent, see KCTractates.h
+ 
+ @param tractateIndex The index of the tractate.
+ @param number The page number.
+ @return An instance of KCDaf.
  */
-
 - (nonnull instancetype)initWithTractateIndex:(NSInteger)tractateIndex andPageNumber:(NSInteger)number;
 
-// MARK: - Displaying a Tractate
+// MARK: - Displaying a Daf
 
 /**
- *  This method returns the display name of a given tractate 
- *  as a Hebrew string.
- *
- *  @return A string with hebrew name of the tractate that corresponds to the tractateIndex.
+ This method returns the display name of a given tractate as a Hebrew string.
+ 
+ @return  A string with hebrew name of the tractate that corresponds to the tractateIndex.
  */
-
 - (nonnull NSString *)name;
 
 /**
- *  This method returns the dispolay name of a given tractate
- *  as a transliterated Hebrew string.
- *
- *  @return A string with transliterated name of the tractate that corresponds to the tractateIndex.
+ This method returns the display name of a given tractate as a transliterated Hebrew string.
+ 
+ @return A string with transliterated name of the tractate that corresponds to the tractateIndex.
  */
-
 - (nonnull NSString *)nameTransliterated;
 
 
