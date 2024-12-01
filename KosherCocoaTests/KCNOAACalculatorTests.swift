bc31b1ddbcdcc767faf8f5a99cfe1c2d8aa341c9
Moshe Berman <mosheberman@users.noreply.github.com>
2024-12-01 15:39:43 -0500

    Update manual `KCNOAACalculatorTests` to use Lakewood NJ, and to define one method per test case.

--- KosherCocoaTests/KCNOAACalculatorTests.swift
+++ KosherCocoaTests/KCNOAACalculatorTests.swift
@@ -12,12 +12,15 @@ import XCTest
 import KosherCocoa
 
 final class KCNOAACalculatorTests: XCTestCase {
-
+    static var timeZone: TimeZone = {
+        TimeZone(identifier: "America/New_York")!
+    }()
     let gregorianCalendar = Calendar(identifier: .gregorian)
     let lakewood: GeoLocation = GeoLocation(
-        latitude: 40.08213,
-        andLongitude: -74.20970,
-        andTimeZone: TimeZone(identifier: "America/New_York")!
+        latitude: 40.096,
+        andLongitude: -74.222,
+        elevation: 25.58,
+        andTimeZone: timeZone
     )
     lazy var lakewoodCalculator:NOAACalculator = {
         NOAACalculator(geoLocation: lakewood)
@@ -39,75 +42,79 @@ final class KCNOAACalculatorTests: XCTestCase {
         XCTAssertEqual(lakewoodCalculator.calculatorName, "US National Oceanic and Atmospheric Administration Algorithm")
     }
     
-    func testCalculatorSunrise() throws {
-        var januaryFirst = DateComponents()
+    func testCalculatesSunriseOnJanuaryFirstAsExpected() throws {
+        var januaryFirst = DateComponents(timeZone: KCNOAACalculatorTests.timeZone)
         januaryFirst.year = 2023
         januaryFirst.month = 1
         januaryFirst.day = 1
-        
-        let calendar = AstronomicalCalendar(location: lakewoodCalculator.geoLocation)
-        calendar.astronomicalCalculator = lakewoodCalculator
+
         calendar.workingDate = gregorianCalendar.date(from: januaryFirst)!
         var sunrise = calendar.sunrise()
-        
+
         januaryFirst.hour = 7
         januaryFirst.minute = 18
-        januaryFirst.second = 57
-        
+        januaryFirst.second = 03
+
         XCTAssertEqual(
             sunrise!.timeIntervalSinceReferenceDate,
             gregorianCalendar.date(from: januaryFirst)!.timeIntervalSinceReferenceDate,
             accuracy: 1
         )
+    }
 
-        var mayFirst = DateComponents()
+    func testCalculatesSunriseOnMayFirstAsExpected() throws {
+        var mayFirst = DateComponents(timeZone: KCNOAACalculatorTests.timeZone)
         mayFirst.year = 2023
         mayFirst.month = 5
         mayFirst.day = 1
-        
+
         calendar.workingDate = gregorianCalendar.date(from: mayFirst)!
-        sunrise = calendar.sunrise()
-        
+        var sunrise = calendar.sunrise()
+
         mayFirst.hour = 5
         mayFirst.minute = 56
-        mayFirst.second = 59
-        
+        mayFirst.second = 6
+
         XCTAssertEqual(
             sunrise!.timeIntervalSinceReferenceDate,
             gregorianCalendar.date(from: mayFirst)!.timeIntervalSinceReferenceDate,
             accuracy: 1
         )
-
+    }
+    func testCalculatesSunriseOnAugustFirstAsExpected() throws {
         var augustFirst = DateComponents()
         augustFirst.year = 2023
         augustFirst.month = 8
         augustFirst.day = 1
-        
+
         calendar.workingDate = gregorianCalendar.date(from: augustFirst)!
-        sunrise = calendar.sunrise()
-        
+        var sunrise = calendar.sunrise()
+
         augustFirst.hour = 5
-        augustFirst.minute = 54
-        augustFirst.second = 51
-        
+        augustFirst.minute = 53
+        augustFirst.second = 56
+
         XCTAssertEqual(
             sunrise!.timeIntervalSinceReferenceDate,
             gregorianCalendar.date(from: augustFirst)!.timeIntervalSinceReferenceDate,
             accuracy: 1
         )
-        
+
+    }
+
+    func testCalculatesSunriseOnDecemberFirstAsExpected() throws {
         var decFirst = DateComponents()
         decFirst.year = 2023
         decFirst.month = 12
         decFirst.day = 1
         
         calendar.workingDate = gregorianCalendar.date(from: decFirst)!
-        sunrise = calendar.sunrise()
-        
+        var sunrise = calendar.sunrise()
+
         decFirst.hour = 6
-        decFirst.minute = 59
-        decFirst.second = 29
-        
+        decFirst.minute = 58
+        decFirst.second = 36
+
         XCTAssertEqual(
             sunrise!.timeIntervalSinceReferenceDate,
             gregorianCalendar.date(from: decFirst)!.timeIntervalSinceReferenceDate,
@@ -115,77 +122,81 @@ final class KCNOAACalculatorTests: XCTestCase {
         )
     }
     
-    func testCalculatorSunset() throws {
-        let lakewoodCalculator = NOAACalculator(geoLocation: GeoLocation(latitude: 40.08213, andLongitude: -74.20970, andTimeZone: TimeZone(identifier: "America/New_York")!))
-        
+    func testCalculatesSunsetOnJanuaryFirstAsExpected() throws {
+
         var januaryFirst = DateComponents()
         januaryFirst.year = 2023
         januaryFirst.month = 1
         januaryFirst.day = 1
-        
-        let calendar = AstronomicalCalendar(location: lakewoodCalculator.geoLocation)
-        calendar.astronomicalCalculator = lakewoodCalculator
+
         calendar.workingDate = gregorianCalendar.date(from: januaryFirst)!
         var sunset = calendar.sunset()
-        
+
         januaryFirst.hour = 16
-        januaryFirst.minute = 41
+        januaryFirst.minute = 42
         januaryFirst.second = 56
-        
+
         XCTAssertEqual(
             sunset!.timeIntervalSinceReferenceDate,
             gregorianCalendar.date(from: januaryFirst)!.timeIntervalSinceReferenceDate,
             accuracy: 1
         )
+    }
 
+    func testCalculatesSunsetOnMayFirstAsExpected() throws {
         var mayFirst = DateComponents()
         mayFirst.year = 2023
         mayFirst.month = 5
         mayFirst.day = 1
-        
+
         calendar.workingDate = gregorianCalendar.date(from: mayFirst)!
-        sunset = calendar.sunset()
-        
+        var sunset = calendar.sunset()
+
         mayFirst.hour = 19
-        mayFirst.minute = 51
+        mayFirst.minute = 52
         mayFirst.second = 33
-        
+
         XCTAssertEqual(
             sunset!.timeIntervalSinceReferenceDate,
             gregorianCalendar.date(from: mayFirst)!.timeIntervalSinceReferenceDate,
             accuracy: 1
         )
+    }
 
+    func testCalculatesSunsetOnAugustFirstAsExpected() throws {
         var augustFirst = DateComponents()
         augustFirst.year = 2023
         augustFirst.month = 8
         augustFirst.day = 1
-        
+
         calendar.workingDate = gregorianCalendar.date(from: augustFirst)!
-        sunset = calendar.sunset()
-        
+        var sunset = calendar.sunset()
+
         augustFirst.hour = 20
-        augustFirst.minute = 10
-        augustFirst.second = 56
-        
+        augustFirst.minute = 11
+        augustFirst.second = 58
+
         XCTAssertEqual(
             sunset!.timeIntervalSinceReferenceDate,
             gregorianCalendar.date(from: augustFirst)!.timeIntervalSinceReferenceDate,
             accuracy: 1
         )
-        
+
+    }
+
+    func testCalculatesSunsetOnDecemberFirstAsExpected() throws {
         var decFirst = DateComponents()
         decFirst.year = 2023
         decFirst.month = 12
         decFirst.day = 1
         
         calendar.workingDate = gregorianCalendar.date(from: decFirst)!
-        sunset = calendar.sunset()
+        var sunset = calendar.sunset()
         
         decFirst.hour = 16
-        decFirst.minute = 31
-        decFirst.second = 56
-        
+        decFirst.minute = 32
+        decFirst.second = 55
+
         XCTAssertEqual(
             sunset!.timeIntervalSinceReferenceDate,
             gregorianCalendar.date(from: decFirst)!.timeIntervalSinceReferenceDate,
